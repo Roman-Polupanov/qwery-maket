6a9555e73237eb20f0fb5b9f2422c2b40e22dc53
Roman Polupanov <87490407+Roman-Polupanov@users.noreply.github.com>
2022-04-09 12:54:10 +0300

    styled

--- src/App.js
+++ src/App.js
@@ -1,75 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
 import './css/index.css'
 import './css/reset.css'
+import { A_link, Body_html, Cover_space, Header_content, Header_q, Main_nav } from './css';
 
 function App() {
   return (
     <div className="App">
-      <div class="cover-space">
-        <header class="header">
-          <div class="header__content">
-            <a class="logo" href="/">
-              <h1 class="logo__title">QWERy</h1>
-            </a>
-            <nav class="main-nav header__center">
-              <ul class="main-nav__list">
-                <li class="main-nav__item">
-                  <a class="main-nav__link main-nav__link_active" href="#">Information</a>
-                </li>
+      <Body_html>
+        <Cover_space>
+          <Header_q>
+            <Header_content>
+              <A_link class="logo" href="/">
+                <h1 class="logo__title">QWERy</h1>
+              </A_link>
+              <Main_nav>
+                <ul class="main-nav__list">
+                  <li class="main-nav__item">
+                    <A_link class="main-nav__link main-nav__link_active" href="#">Information</A_link>
+                  </li>
 
-                <li>
-                  <a class="main-nav__link" href="#">About us</a>
-                </li>
+                  <li>
+                    <A_link class="main-nav__link" href="#">About us</A_link>
+                  </li>
 
-                <li>
-                  <a class="main-nav__link" href="#">Culture</a>
+                  <li>
+                    <A_link class="main-nav__link" href="#">Culture</A_link>
+                  </li>
+                </ul>
+              </Main_nav>
+              <label for="name" class="search">
+                <div class="search__icon" src="#"></div>
+                <input class="search__form" type="text" placeholder="Search" />
+              </label>
+            </Header_content>
+          </Header_q>
+        </Cover_space>
+
+        <section class="section section-main">
+          <div class="section-main">
+            <div class="main-title">
+              <div class="main-title__summary">Explore the Nature Beauty
+              </div>
+              <h1 class="main-title__text">Discover
+                <span class="main-title__text_regular">Wonderful Indonesia</span>
+              </h1>
+            </div>
+            <div class="social-networks">
+              <h3 class="social-networks__title">Follow us</h3>
+              <ul class="social-networks__list">
+                <li class="social-networks__item">
+                  <a class="social-networks__link" href="#">
+                    <div class="icon__instagram"></div>
+                  </a>
+                </li>
+                <li class="social-networks__item">
+                  <a class="social-networks__link" href="#">
+                    <div class="icon__twitter"></div>
+                  </a>
+                </li>
+                <li class="social-networks__item">
+                  <a class="social-networks__link" href="#">
+                    <div class="icon__facebook"></div>
+                  </a>
                 </li>
               </ul>
-            </nav>
-            <label for="name" class="search">
-              <div class="search__icon" src="#"></div>
-              <input class="search__form" type="text" placeholder="Search" />
-            </label>
-          </div>
-        </header>
-      </div>
-
-      <section class="section section-main">
-        <div class="section-main">
-          <div class="main-title">
-            <div class="main-title__summary">Explore the Nature Beauty
             </div>
-            <h1 class="main-title__text">Discover
-              <span class="main-title__text_regular">Wonderful Indonesia</span>
-            </h1>
+            <div class="scroll-down">
+              Scroll down
+              <div class="scroll-down__icon icon__arrow_downward"></div>
+            </div>
           </div>
-          <div class="social-networks">
-            <h3 class="social-networks__title">Follow us</h3>
-            <ul class="social-networks__list">
-              <li class="social-networks__item">
-                <a class="social-networks__link" href="#">
-                  <div class="icon__instagram"></div>
-                </a>
-              </li>
-              <li class="social-networks__item">
-                <a class="social-networks__link" href="#">
-                  <div class="icon__twitter"></div>
-                </a>
-              </li>
-              <li class="social-networks__item">
-                <a class="social-networks__link" href="#">
-                  <div class="icon__facebook"></div>
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div class="scroll-down">
-            Scroll down
-            <div class="scroll-down__icon icon__arrow_downward"></div>
-          </div>
-        </div>
-      </section>
+        </section>
+
+      </Body_html>
+
     </div>
   );
 }
